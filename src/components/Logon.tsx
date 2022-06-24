a84018148fa674c7f041b480b7c1f5f2316c96cb
root <root@NT007294.pm.govrn>
2022-06-24 10:34:34 -0300

    Feat: adjust

--- src/components/Logon.tsx
+++ src/components/Logon.tsx
@@ -1,6 +1,6 @@
-import { FaSignInAlt } from 'react-icons/fa'
+import Link from 'next/link';
+import { FaSignInAlt } from 'react-icons/fa';
 import styles from '../styles/components/Logon.module.scss';
-import Link from 'next/link'
 
 export function Logon() {
     return (
@@ -8,18 +8,14 @@ export function Logon() {
             <section className={styles.form}>
                 <img src="/icons/death-star.svg" />
                 <form>
-                    <h1>
-                        Faça seu Logon
-                    </h1>
-                    <input placeholder="Sua ID" />
                     <Link href="/Lists">
-                        <button className={styles.button} type="submit">Entrar</button>
+                        <button className={styles.button} type="submit">Começar</button>
                     </Link>
 
                     <Link href="/form">
                         <div className={styles.backLink}>
                             <FaSignInAlt size={16} />
-                            Não tenho cadastro
+                            ~form teste~
                         </div>
                     </Link>
                 </form>
